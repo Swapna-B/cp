812ab933c93260e409c3212f18ef75b1d9ba5a1e
Swapna-B <swapna.dadigala@gmail.com>
2023-03-18 12:22:47 +0530

    asdm

--- print.c
+++ print.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 void main()
 {
+    int a=5,b=56;
     printf("hi");
+    printf("%d",a+b);
+      printf("%d",a-b);
+        printf("%d",a*b);
+          printf("%d",a/b);  
+    
 }
